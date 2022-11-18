e86f0ab5eadde5e2c964bee2403c0d8e91539c21
leycec <leycec@gmail.com>
2022-11-18 03:43:30 -0400

    Streamlit Cloud + `PYTHONPATH`.
    This commit resolves a critical `PYTHONPATH` (i.e., `sys.path`) issue
    when hosting this app on Streamlit Cloud. Specifically, this commit adds
    a shameless (albeit well-tested) kludge to the head of the
    `calculion.main` entry point forcefully injecting the parent directory
    of the top-level `calculion/` directory defining this app's top-level
    package to the `sys.path` list if that list does *NOT* already contain
    this directory, which it does not when run either from Streamlit Cloud
    or as `command streamlit run calculion/main.py` rather than as
    `python -m streamlit run calculion/main.py`. In theory, our app should
    now deploy without complaint to Streamlit Cloud.
    (*Meticulous meritocracy of cray-cray crayfish!*)

--- calculion/main.py
+++ calculion/main.py
@@ -16,6 +16,63 @@ Specifically, this submodule is imported by:
 * Integration tests programmatically exercising app functionality.
 '''
 
+# ....................{ KLUDGES ~ path                     }....................
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# CAUTION: Kludge PYTHONPATH *BEFORE* importing from this package below.
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Explicitly register all files and subdirectories of the parent directory
+# containing this module to be importable modules and packages (respectively)
+# for the remainder of this Python process if this directory has yet to be
+# registered.
+#
+# Technically, this should *NOT* be required. Streamlit should implicitly
+# guarantee this to be the case. Indeed, when Streamlit is run as a module
+# (e.g., as "python3 -m streamlit run {app_name}/main.py"), Streamlit does just
+# that. Unfortunately, when Streamlit is run as an external command (e.g., as
+# "streamlit run {app_name}/main.py"), Streamlit does *NOT* guarantee this to be
+# the case. Since Streamlit Cloud runs Streamlit as an external command rather
+# than as a module, Streamlit Cloud effectively does *NOT* guarantee this to be
+# the case as well.
+
+# Isolate this kludge to a private function for safety.
+def _register_dir() -> None:
+
+    # Defer kludge-specific imports. Avert thy eyes, purist Pythonistas!
+    from logging import info
+    from pathlib import Path
+    from sys import path as sys_path
+
+    # Log this detection attempt.
+    info('[APP] Detecting whether app package directory requires registration on "sys.path": %s', sys_path)
+    # print('Registering app package directory for importation: %s')
+
+    # Path object encapsulating the absolute filename of the file defining the
+    # current module. Note that doing so may raise either:
+    # * If this file inexplicably does *NOT* exist, "FileNotFoundError".
+    # * If this file inexplicably resides under a directory subject to an
+    #   infinite symbolic link loop, "RuntimeError".
+    main_file = Path(__file__).resolve(strict=True)
+
+    # Absolute dirname of the parent directory containing this app's top-level
+    # package, which is guaranteed to be either:
+    # * If this app is currently installed editably (e.g., "pip install -e ."),
+    #   the repository directory containing the ".git/" directory for this app.
+    # * If this app is currently installed non-editably (e.g., "pip install ."),
+    #   the equivalent of the "site-packages/" directory for the active Python.
+    package_dirname = str(main_file.parents[1])
+
+    # If the current PYTHONPATH does *NOT* already contain this directory...
+    if package_dirname not in sys_path:
+        # Log this registration attempt.
+        info('[APP] Registering app package directory for importation: %s', package_dirname)
+        # print('Registering app package directory for importation: %s')
+
+        # Append this directory to the current PYTHONPATH.
+        sys_path.append(package_dirname)
+
+# Kludge us up the bomb.
+_register_dir()
+
 # ....................{ IMPORTS                            }....................
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # CAUTION: Avoid importing anything at module scope *EXCEPT* from official
